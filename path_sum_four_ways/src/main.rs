7a8873d8599f5e46b330c7763cd47e0f06872ccf
Isaac Cloos <cloosisaac@gmail.com>
2022-06-01 23:37:41 -0400

    success!!!!

--- path_sum_four_ways/src/main.rs
+++ path_sum_four_ways/src/main.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashMap, fs, hash::Hash, ops::Add, str::FromStr};
+use std::{collections::HashSet, fs, hash::Hash, ops::Add, str::FromStr};
 
-const DATA_PATH: &str = "data_easy.txt";
+const DATA_PATH: &str = "data.txt";
 
 fn main() {
     let unparsed_string = fs::read_to_string(DATA_PATH).expect("Inaccurate data path.");
@@ -30,7 +30,14 @@ type Matrix<T> = Vec<Vec<T>>;
 
 trait Table<T> {
     fn from_table_string(unparsed_string: String, delimiter: char) -> Self;
+    /// Return value from the cell coordinates
     fn get_value(&self, cell: &Cell) -> T;
+    /// Set value at the cell coordinates
+    fn set_value(&mut self, cell: &Cell, value: T);
+    /// Get movement options for a given cell
+    ///
+    /// An off limits list is provided to limit redundancy and infinite loops
+    fn get_options(&self, cell: &Cell, off_limits: &HashSet<Cell>) -> Vec<Cell>;
     /// Returns cumulative cost to travel from start to end
     fn calculate_distance(&self, start: Cell, end: Cell) -> T;
     /// Coordinates for first item of first array of matrix
@@ -41,7 +48,7 @@ trait Table<T> {
 
 impl<T> Table<T> for Matrix<T>
 where
-    T: FromStr + Copy + Add,
+    T: FromStr + Copy + Add<Output = T> + Ord,
 {
     fn from_table_string(unparsed_string: String, delimiter: char) -> Self {
         unparsed_string
@@ -67,15 +74,82 @@ where
             .expect("x value out of range")
     }
 
-    fn calculate_distance(&self, start: Cell, end: Cell) -> T {
-        let mut shortest_path_map = HashMap::<Cell, T>::from([(start, self.get_value(&start))]);
-        
+    fn set_value(&mut self, cell: &Cell, value: T) {
+        self[cell.y][cell.x] = value
+    }
 
-        while !shortest_path_map.contains_key(&end) {
-            shortest_path_map.insert(end, self.get_value(&end));
+    fn get_options(&self, cell: &Cell, off_limits: &HashSet<Cell>) -> Vec<Cell> {
+        let mut result = Vec::new();
+
+        // up is available
+        if cell.y > 0 {
+            result.push(Cell {
+                x: cell.x,
+                y: cell.y - 1,
+            });
         }
 
-        shortest_path_map[&end]
+        // down is available
+        if cell.y < self.len() - 1 {
+            result.push(Cell {
+                x: cell.x,
+                y: cell.y + 1,
+            });
+        }
+
+        // left is available
+        if cell.x > 0 {
+            result.push(Cell {
+                x: cell.x - 1,
+                y: cell.y,
+            });
+        }
+
+        // right is available
+        if cell.x < self[cell.y].len() - 1 {
+            result.push(Cell {
+                x: cell.x + 1,
+                y: cell.y,
+            });
+        }
+
+        result
+            .into_iter()
+            .filter(|c| !off_limits.contains(c))
+            .collect::<Vec<Cell>>()
+    }
+
+    fn calculate_distance(&self, start: Cell, end: Cell) -> T {
+        let mut working_matrix = self.clone();
+        let mut shortest_path_set = HashSet::<Cell>::from([start]);
+
+        while !shortest_path_set.contains(&end) {
+            let mut options_per_cycle = Vec::<(Cell, T)>::new();
+
+            for cell in shortest_path_set.iter() {
+                let cell_val = working_matrix.get_value(cell);
+
+                match working_matrix
+                    .get_options(cell, &shortest_path_set)
+                    .iter()
+                    .map(|c| (*c, working_matrix.get_value(c) + cell_val))
+                    .min_by(|a, b| a.1.cmp(&b.1))
+                {
+                    Some(next_step) => options_per_cycle.push(next_step),
+                    None => (), // pass, no next steps available
+                }
+            }
+
+            let cheapest_next_step = options_per_cycle
+                .iter()
+                .min_by(|a, b| a.1.cmp(&b.1))
+                .unwrap();
+
+            working_matrix.set_value(&cheapest_next_step.0, cheapest_next_step.1);
+            shortest_path_set.insert(cheapest_next_step.0);
+        }
+
+        working_matrix.get_value(&end)
     }
 
     fn get_origin(&self) -> Cell {
